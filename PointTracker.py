6de855a824984832b3a985f11a44cfc1eff1b328
Jaxson098 <jaxson.paige@icloud.com>
2024-07-30 04:46:54 -1000

    cleanup

--- PointTracker.py
+++ PointTracker.py
@@ -4,7 +4,6 @@ import keyboard
 import time
 import json
 import sys
-import subprocess
 
 #for debugging
 print("started")
@@ -15,179 +14,85 @@ b = 0
 running = False
 elapsedTime = 0
 ports = {}
+portAmount = 0
 
-#load the json file so the values can be incremnted
-with open('/home/jaxson/dronebeacon-board/game_state.json', 'r') as game_state:
-    scores = json.load(game_state)
+def updatePoints(r,b,elapsedTime):
+    #load the json file so the values can be incremnted
+    with open('/home/jaxson/AutoPointTracker/game_state.json', 'r') as game_state:
+        scores = json.load(game_state)
 
-scores["red"] = r
-scores["blue"] = b
-scores["gameTime"] = 180-elapsedTime
+    scores["red"] = r
+    scores["blue"] = b
+    scores["gameTime"] = round(180-elapsedTime)
 
-#reset scores and gma etime to zero (they may not be because of past games)
-with open('/home/jaxson/dronebeacon-board/game_state.json', 'w') as game_state:
-    json.dump(scores, game_state)
+    with open('/home/jaxson/AutoPointTracker/game_state.json', 'w') as game_state:
+        json.dump(scores, game_state)
 
-# ser0 = "nothing"
-# ser1 = "nothing"
-# ser2 = "nothing"
-# ser3 = "nothing"
-# ser4 = "nothing"
-# ser5 = "nothing"
-# ser6 = "nothing"
-# ser7 = "nothing"
+def getPorts():
+    for i in range(10):
+        serialPort = f"/dev/ttyUSB{i}"
+        try:
+            ports[str(i)] = serial.Serial(serialPort, 9600, timeout = 0.001)
+            print(f"beacon {i} connected")
+        except serial.SerialException as e:
+            print(f"beacon {i} not connected")
 
-# data0 = "nothing"
-# data1 = "nothing"
-# data2 = "nothing"
-# data3 = "nothing"
-# data4 = "nothing"
-# data5 = "nothing"
-# data6 = "nothing"
-# data7 = "nothing"
+def getScores():
+    global b
+    global r
+    for i  in ports:
+        ser = ports[str(i)]
+        data = ser.readline().decode('utf-8', errors='ignore').strip()
+        if data == "BLUE":
+            b += 1
+            print(f"incremented blue to {b}")
 
-# isBlue = [
-#     data0 == "BLUE",
-#     data1 == "BLUE",
-#     data2 == "BLUE",
-#     data3 == "BLUE",
-#     data4 == "BLUE",
-#     data5 == "BLUE",
-#     data6 == "BLUE",
-#     data7 == "BLUE"
-#         ]
+        elif data == "RED":
+            r += 1  
+            print(f"incremented red to {r}")
+    return b, r, 
 
-# isRed = [
-#     data0 == "RED",
-#     data1 == "RED",
-#     data2 == "RED",
-#     data3 == "RED",
-#     data4 == "RED",
-#     data5 == "RED",
-#     data6 == "RED",
-#     data7 == "RED"
-#         ]
-
-# try:
-#     ser0 = serial.Serial("/dev/ttyUSB0", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected (ser0)")
-# try:
-#     ser1 = serial.Serial("/dev/ttyUSB1", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser1")
-# try:
-#     ser2 = serial.Serial("/dev/ttyUSB2", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser2")
-# try:
-#     ser3 = serial.Serial("/dev/ttyUSB3", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser3")
-# try:
-#     ser4 = serial.Serial("/dev/ttyUSB4", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser4")
-# try:
-#     ser5 = serial.Serial("/dev/ttyUSB5", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser5")
-# try:
-#     ser6 = serial.Serial("/dev/ttyUSB6", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser6")
-# try:
-#     ser7 = serial.Serial("/dev/ttyUSB7", 9600, timeout = 1)
-# except serial.SerialException as e:
-#     print("not all beacons connected ser7")
+def resetBeacons():
+    global ports
+    startB = dict(list(ports.items())[len(ports)//2:])
+    startR = dict(list(ports.items())[:len(ports)//2])
+    print(f"\nstartB = {startB}\n")
+    print(f"\nstartR = {startR}\n")
 
 while True:
     if keyboard.is_pressed('esc'):
         print("clicked esc running sys.exit()...")
         sys.exit()
+
     if keyboard.is_pressed('space'):
-
-        for i in range(10):
-            serialPort = f"/dev/ttyUSB{i}"
-            try:
-                ports[str(i)] = serial.Serial(serialPort, 9600, timeout = 0.001)
-            except serial.SerialException as e:
-                print(f"beacon {i} not connected")
-
-        print(ports)
-
+        
+        getPorts()
+        resetBeacons()
         running = True
         print("space pressed")
         startTime = time.time()
         elapsedTime = 0
         r = 0
         b = 0
-        scores["red"] = r
-        scores["blue"] = b
-        scores["gameTime"] = 180-elapsedTime
-        with open('/home/jaxson/dronebeacon-board/game_state.json', 'w') as game_state:
-            json.dump(scores, game_state)
+        
+        updatePoints(r,b,elapsedTime)
 
     while running and elapsedTime <= 180:
 
-        for i in ports:
-            ser = ports[str(i)]
-            data = ser.readline().decode('utf-8', errors='ignore').strip()
-            if data == "BLUE":
-                b += 1
-                print(f"incremented blue to {b}")
-                scores["blue"] = b
-
-            if data == "RED":
-                r += 1  
-                print(f"incremented red to {r}")
-                scores["red"] = r
-
-        # if ser0 != "nothing":
-        #     data0 = ser0.readline().decode('utf-8', errors='ignore').strip()
-        # if ser1 != "nothing":
-        #     data1 = ser1.readline().decode('utf-8', errors='ignorestrip()').
-        # if ser2 != "nothing":
-        #     data2 = ser2.readline().decode('utf-8', errors='ignore').strip()
-        # if ser3 != "nothing":
-        #     data3 = ser3.readline().decode('utf-8', errors='ignore').strip()
-        # if ser4 != "nothing":
-        #     data4 = ser4.readline().decode('utf-8', errors='ignore').strip()
-        # if ser5 != "nothing":
-        #     data5 = ser5.readline().decode('utf-8', errors='ignore').strip()
-        # if ser6 != "nothing":
-        #     data6 = ser6.readline().decode('utf-8', errors='ignore').strip()
-        # if ser7 != "nothing":
-        #     data7 = ser7.readline().decode('utf-8', errors='ignore').strip()
-
-        # if data1 == "BLUE":
-        #     b += 1
-        #     print(f"incremented blue to {b}")
-        #     scores["blue"] = b
-
-        # if data0 == "RED":
-        #     r += 1
-        #     print(f"incremented red to {r}")
-        #     scores["red"] = r
+        getScores() 
 
         currentTime = time.time()
         elapsedTime = currentTime - startTime
         
-        scores["gameTime"] = round(180-elapsedTime)
-
-        with open('/home/jaxson/dronebeacon-board/game_state.json', 'w') as game_state:
-            json.dump(scores, game_state)
+        updatePoints(r,b,elapsedTime)
 
         if keyboard.is_pressed('enter'):
             print("elapsed time > 180")
+
             r = 0
             b = 0
             elapsedTime = 0
-            scores["red"] = r
-            scores["blue"] = b
-            scores["gameTime"] = 180-elapsedTime
-            with open('/home/jaxson/dronebeacon-board/game_state.json', 'w') as game_state:
-                json.dump(scores, game_state)
+            updatePoints(r,b,elapsedTime)
             running = False
             break
 
